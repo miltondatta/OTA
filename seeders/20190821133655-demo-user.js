a82ef52c578f66fb321c548f5522e36b36bc18f7
shahadat_cloud <shahadat@clouditbd.com>
2019-08-21 19:55:41 +0600

    update seeder

---
+++ seeders/20190821133655-demo-user.js
@@ -0,0 +1,30 @@
+'use strict';
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+
+      //Add altering commands here.
+      //Return a promise to correctly handle asynchronicity.
+
+      //Example:
+
+      return queryInterface.bulkInsert('Users', [{
+        name: 'John Doe',
+        email: 'demo@demo.com',
+        password: '123456',
+        mobile: '01551807064',
+        createdAt: new Date(),
+        updatedAt: new Date()
+      }], {});
+  },
+
+  down: (queryInterface, Sequelize) => {
+    /*
+      Add reverting commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example: */
+
+      return queryInterface.bulkDelete('Users', null, {});
+  }
+};
