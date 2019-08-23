51ec20add4a0ed754aadba904a7edf91f923065e
Shahadat Hossain <shahadat.kuet08@gmail.com>
2019-08-23 16:30:05 +0600

    update dependency manager

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,23 +1,23 @@
-const {sequelize, Sequelize}     = require('../models/index');
-const user                       = require('../models').user;
-const bcrypt                     = require('bcryptjs');
-const jwt                        = require('jsonwebtoken');
+const { sequelize, Sequelize } = require('../models/index');
+const user = require('../models').user;
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
-exports.store = async (req, res) => {
-    const { name, email, password, mobile } =  req.body;
+exports.store = async(req, res) => {
+    const { name, email, password, mobile } = req.body;
 
     user.findOne({ where: { email } }).then(results => {
-      if(results) {
-          return res.status(400).json({ msg: 'Email already exist!' });
-      } else {
-          bcrypt.genSalt(10, (err, salt) => {
-              bcrypt.hash(password, salt, (err, hash) => {
-                  user.create({ name:name, email:email, password:hash, mobile:mobile }).then(results => {
-                      return res.status(200).json({ msg: 'User created successfully!' });
-                  });
-              });
-          });
-      }
+        if (results) {
+            return res.status(400).json({ msg: 'Email already exist!' });
+        } else {
+            bcrypt.genSalt(10, (err, salt) => {
+                bcrypt.hash(password, salt, (err, hash) => {
+                    user.create({ name: name, email: email, password: hash, mobile: mobile }).then(results => {
+                        return res.status(200).json({ msg: 'User created successfully!' });
+                    });
+                });
+            });
+        }
     });
 
     /*
@@ -37,30 +37,29 @@ exports.store = async (req, res) => {
 };
 
 
-exports.login = async (req, res) => {
-    const {email, password } =  req.body;
+exports.login = async(req, res) => {
+    const { email, password } = req.body;
     user.findOne({ where: { email } }).then(user => {
-        if(!user) {
+        if (!user) {
             return res.status(400).json({ msg: 'Email not found!' });
         }
 
         //Check password
         bcrypt.compare(password, user.password).then(isMatch => {
-           if(isMatch){
-               const payload    =   { id:user.id, name:user.name, email:user.email };
-               jwt.sign(
-                   payload,
-                   'secret',
-                   { expiresIn: 3600 },
-                   (err, token) => {
-                       res.json({
-                           success:true,
-                           token:'ptm' + token
-                       });
-                   });
-           } else {
-               return res.json({ msg: 'Your password is incorrect!' });
-           }
+            if (isMatch) {
+                const payload = { id: user.id, name: user.name, email: user.email };
+                jwt.sign(
+                    payload,
+                    'secret', { expiresIn: 3600 },
+                    (err, token) => {
+                        res.json({
+                            success: true,
+                            token: 'ptm' + token
+                        });
+                    });
+            } else {
+                return res.json({ msg: 'Your password is incorrect!' });
+            }
         });
     });
 };
