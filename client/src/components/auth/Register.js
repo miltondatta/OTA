eedc0d8905ed38d7e6b0836c55e3ceb7aab6a4b3
shahadat_cloud <shahadat@clouditbd.com>
2019-08-26 18:19:01 +0600

    added link

---
+++ client/src/components/auth/Register.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+ export class Register extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
+
+
+export default Register
