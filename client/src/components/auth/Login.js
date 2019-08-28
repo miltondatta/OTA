2f149cbdc16bf47485a23361d91090ae82e0d3bb
shahadat_cloud <shahadat@clouditbd.com>
2019-08-28 19:28:34 +0600

    update login logout and reg

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -1,30 +1,72 @@
 import React, { Component, Fragment } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { loginUser } from '../../actions/authActions';
 
 
 class Login extends Component {
+
+    constructor() {
+        super();
+        this.state = {
+            email: '',
+            password: '',
+            errors: {}
+        };
+
+        this.onChange = this.onChange.bind(this);
+        this.onSubmit = this.onSubmit.bind(this);
+    }
+
+    onChange(e) {
+        this.setState({ [e.target.name]: e.target.value });
+    }
+
+    onSubmit(e) {
+        e.preventDefault();
+
+        const userData = {
+            email: this.state.email,
+            password: this.state.password
+        }
+
+        this.props.loginUser(userData);
+    }
+
+
     render() {
         return (
             <Fragment>
                 <div className="container breadcrub"></div>
                 <div className="login-fullwidith">
-                    <div className="login-wrap">
-                        <div className="login-c1">
-                            <div className="cpadding50">
-                                <input type="text" className="form-control logpadding" placeholder="Email" required/>
-                                <br />
-                                <input type="text" className="form-control logpadding" placeholder="Password" required/>
+                    <form onSubmit={this.onSubmit}>
+                        <div className="login-wrap">
+                            <div className="login-c1">
+                                <div className="cpadding50">
+                                    <input type="email" className="form-control logpadding"
+                                        name="email" value={this.state.email}
+                                        onChange={this.onChange}
+                                        placeholder="Enter Your Email" required /> <br />
+
+                                    <input type="password" className="form-control logpadding"
+                                        name="password" value={this.state.password}
+                                        onChange={this.onChange}
+                                        minLength="5"
+                                        placeholder="Enter Your Password" required /> <br />
+
+                                </div>
                             </div>
-                        </div>
-                        <div className="login-c2">
-                            <div className="logmargfix">
-                                <div className="chpadding50">
-                                    <div className="alignbottom">
-                                        <button className="btn-search4" type="submit">Submit</button>
+                            <div className="login-c2">
+                                <div className="logmargfix">
+                                    <div className="chpadding50">
+                                        <div className="alignbottom">
+                                            <button className="btn-search4" type="submit">Submit</button>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
                         </div>
-                    </div>
+                    </form>
                 </div>
             </Fragment>
         )
@@ -33,4 +75,15 @@ class Login extends Component {
 
 
 
-export default Login
+Login.propTypes = {
+    loginUser: PropTypes.func.isRequired,
+    auth: PropTypes.object.isRequired,
+    errors: PropTypes.object.isRequired
+  };
+  
+  const mapStateToProps = state => ({
+    auth: state.auth,
+    errors: state.errors
+  });
+  
+  export default connect(mapStateToProps, { loginUser })(Login);
