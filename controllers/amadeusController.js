fc1d2681c3d903dd89d607b26ab16e864de7fa70
shahadat_cloud <shahadat@clouditbd.com>
2019-09-01 20:07:07 +0600

    added amadeus controller

--- controllers/amadeusController.js
+++ controllers/amadeusController.js
@@ -7,23 +7,15 @@ var amadeus = new Amadeus({
     clientSecret: process.env.AMADEUS_CLIENT_SECRET
 });
 
+
+
 exports.locations = async (req, res) => {
-
-
     amadeus.referenceData.locations.get({
         keyword: 'LON',
         subType: Amadeus.location.any
     }).then(function (response) {
-       return res.json(response);
+        return res.json(response);
     }).catch(function (responseError) {
         return res.json(responseError);
     });
-
-
-    
-
-
-
-   // return res.json({ 'msg': 'amadeus' });
-
 };
