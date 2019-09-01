897fbd45fb43008814b2a4a61d1106e1afac663c
plaboncosta <plabonjoseph@gmail.com>
2019-09-01 19:09:27 +0600

    flight search list page demo design done

--- client/src/components/flights/SearchFlight.js
+++ client/src/components/flights/SearchFlight.js
@@ -1,6 +1,15 @@
 import React, {Component} from 'react'
+import DatePicker from "react-datepicker";
 
 class SearchFlight extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            departure: new Date(),
+            return: new Date()
+        };
+    }
+
     render() {
         return (
             <div className="hotelstab2">
@@ -13,26 +22,52 @@ class SearchFlight extends Component {
 
                 <div className="wh100percent">
                     <div className="wh100percent textleft">
-                        <span className="opensans size13">To</span>
+                        <span className="opensans size13">Flying To</span>
                         <input type="text" className="form-control" placeholder="City or airport"/>
                     </div>
                 </div>
-                <div className="clearfix pbottom15"></div>
+                <div className="clearfix pbottom15"> </div>
                 <div className="w50percent">
                     <div className="wh90percent textleft">
-                        <span className="opensans size13">Departing</span>
-                        <input type="text" className="form-control" id="datepicker3"
-                               placeholder="mm/dd/yyyy"/>
+                        <span className="opensans size13">Departure</span>
+                        <DatePicker className="form-control mySelectBoxclassName"
+                                    selected={this.state.departure}
+                                    onChange={date => this.setState({departure: date})}/>
                     </div>
                 </div>
+                {this.props.roundTrip &&
+                    <div className="w50percentlast">
+                        <div className="wh90percent textleft right">
+                            <span className="opensans size13">Return</span>
+                            <DatePicker className="form-control mySelectBoxclassName"
+                                        selected={this.state.return}
+                                        onChange={date => this.setState({return: date})}/>
+                        </div>
+                    </div>
+                }
+                {this.props.oneWay &&
                 <div className="w50percentlast">
                     <div className="wh90percent textleft right">
-                        <span className="opensans size13">Returning</span>
-                        <input type="text" className="form-control" id="datepicker4"
-                               placeholder="mm/dd/yyyy"/>
+                        <span className="opensans size13">Class</span>
+                        <select name="economy_class" className="form-control mySelectBoxclassName">
+                            <option value="1">Economy Class</option>
+                            <option value="2">Business Class</option>
+                        </select>
                     </div>
                 </div>
-                <div className="clearfix pbottom15"></div>
+                }
+                {this.props.roundTrip &&
+                <div className="w50percent marginTop5">
+                    <div className="wh90percent textleft">
+                        <span className="opensans size13">Departure</span>
+                        <select name="economy_class" className="form-control mySelectBoxclassName">
+                            <option value="1">Economy Class</option>
+                            <option value="2">Business Class</option>
+                        </select>
+                    </div>
+                </div>
+                }
+                <div className="clearfix pbottom15"> </div>
                 <div className="room1">
                     <div className="w50percent">
                         <div className="wh90percent textleft">
@@ -60,9 +95,24 @@ class SearchFlight extends Component {
                         </div>
                     </div>
                 </div>
-                <div className="clearfix"></div>
+                <div className="clearfix"> </div>
                 <br/>
-                <button type="submit" className="btn-search3">Search</button>
+                <div className="room1">
+                    <div className="w50percent">
+                        <div className="wh90percent textleft">
+                            <span className="opensans size13">Child</span>
+                            <select className="form-control mySelectBoxClass">
+                                <option selected>0</option>
+                                <option>1</option>
+                            </select>
+                        </div>
+                    </div>
+                    <div className="w50percentlast">
+                        <div className="wh90percent textleft right">
+                            <button type="submit" className="btn-search3">Search</button>
+                        </div>
+                    </div>
+                </div>
             </div>
         )
     }
