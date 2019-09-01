36a7da733f1e8141b749a3708686b6bd4b98599c
shahadat_cloud <shahadat@clouditbd.com>
2019-09-01 11:48:31 +0600

    add flight payment and list page

--- client/src/App.js
+++ client/src/App.js
@@ -12,6 +12,8 @@ import Landing from './components/layout/Landing';
 import Footer from './components/layout/Footer';
 import Login from './components/auth/Login';
 import Register from './components/auth/Register';
+import FlightList from './components/flights/FlightList';
+import FlightPayment from './components/payments/FlightPayment';
 
 
 // Check for token
@@ -43,6 +45,8 @@ function App() {
         <Route exact path='/' component={Landing}/>
         <Route exact path='/login' component={Login}/>
         <Route exact path='/register' component={Register}/>
+        <Route exact path='/flight-list' component={FlightList}/>
+        <Route exact path='/flight-payment' component={FlightPayment}/>
      <Footer/>   
    </Router>
    </Provider>
