0d17aa9f601c087072384e325ea4e6809c81ff09
plaboncosta <plabonjoseph@gmail.com>
2019-09-02 11:36:55 +0600

    add airport database

--- migrations/20190822055618-create-user.js
+++ migrations/20190822055618-create-user.js
@@ -4,9 +4,7 @@ module.exports = {
     return queryInterface.createTable('users', {
       id: {
         allowNull: false,
-        autoIncrement: true,
-        primaryKey: true,
-        type: Sequelize.INTEGER
+        type: Sequelize.STRING
       },
       name: {
         type: Sequelize.STRING
---
+++ models/airport.js
@@ -0,0 +1,28 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  const airport = sequelize.define('airport', {
+    ident: DataTypes.STRING,
+    type: DataTypes.STRING,
+    name: DataTypes.STRING,
+    latitude_deg: DataTypes.INTEGER,
+    longitude_deg: DataTypes.STRING,
+    elevation_ft: DataTypes.INTEGER,
+    continent: DataTypes.STRING,
+    iso_country: DataTypes.STRING,
+    iso_region: DataTypes.STRING,
+    municipality: DataTypes.STRING,
+    scheduled_service: DataTypes.INTEGER,
+    gps_code: DataTypes.STRING,
+    iata_code: DataTypes.STRING,
+    local_code: DataTypes.STRING,
+    home_link: DataTypes.STRING,
+    wikipedia_link: DataTypes.STRING,
+    keywords: DataTypes.STRING,
+    score: DataTypes.INTEGER,
+    last_updated: DataTypes.DATE
+  }, {});
+  airport.associate = function(models) {
+    // associations can be defined here
+  };
+  return airport;
+};
