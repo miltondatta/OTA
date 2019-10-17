74757bd31dd9f6cb5b941dac30490b580597ace4
plaboncosta <plabonjoseph@gmail.com>
2019-10-17 18:30:57 +0600

    format

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,15 +1,16 @@
-import React, { Component, Fragment } from 'react';
+import React, {Component, Fragment} from 'react';
 import PropTypes from 'prop-types';
-import { withRouter } from 'react-router-dom';
+import {withRouter} from 'react-router-dom';
+
+// Css
 import '../../assets/css/register.css';
 
 // Redux
-import { connect } from 'react-redux';
-import { registerUser } from '../../actions/authActions';
+import {connect} from 'react-redux';
+import {registerUser} from '../../actions/authActions';
 
 class Register extends Component {
-
-    constructor(props){
+    constructor(props) {
         super(props);
         this.state = {
             name: '',
@@ -21,43 +22,43 @@ class Register extends Component {
             pass_match: true
         };
 
-        this.onChange   =   this.onChange.bind(this);
-        this.onSubmit   =   this.onSubmit.bind(this);
+        this.onChange = this.onChange.bind(this);
+        this.onSubmit = this.onSubmit.bind(this);
     }
 
     componentDidMount() {
         if (this.props.auth.isAuthenticated) {
-          this.props.history.push('/');
+            this.props.history.push('/');
         }
-      }
-    
-      componentWillReceiveProps(nextProps) {
+    }
+
+    componentWillReceiveProps(nextProps) {
         if (nextProps.errors) {
-          this.setState({ errors: nextProps.errors });
+            this.setState({errors: nextProps.errors});
         }
-      }
-
-
-    onChange(e){
-        this.setState({ [e.target.name]: e.target.value });
     }
 
 
-    onSubmit(e){
+    onChange(e) {
+        this.setState({[e.target.name]: e.target.value});
+    }
+
+
+    onSubmit(e) {
         e.preventDefault();
 
         // Check Confirm Password
-        if(this.state.password2 !== '') {
+        if (this.state.password2 !== '') {
             const password = this.state.password;
             const password2 = this.state.password2;
-            if(password !== password2) {
+            if (password !== password2) {
                 this.setState({pass_match: false});
                 return;
             }
-            if(password === password2) this.setState({pass_match: true});
+            if (password === password2) this.setState({pass_match: true});
         }
 
-        const newUser   =   {
+        const newUser = {
             name: this.state.name,
             email: this.state.email,
             mobile: this.state.mobile,
@@ -73,36 +74,36 @@ class Register extends Component {
         const password_matched = this.state.pass_match ? '' : 'password_not_matched';
         return (
             <Fragment>
-                <div className="container breadcrub"> </div>
+                <div className="container breadcrub"></div>
                 <div className="register-fullwidith">
                     <form onSubmit={this.onSubmit}>
                         <div className="register-wrap">
                             <div className="register-c1">
                                 <div className="cpadding50">
                                     <input type="text" className="form-control logpadding"
-                                        name="name" value={this.state.name}
-                                        onChange={this.onChange}
-                                        placeholder="Enter Name" required /> <br />
+                                           name="name" value={this.state.name}
+                                           onChange={this.onChange}
+                                           placeholder="Enter Name" required/> <br/>
 
                                     <input type="text" className="form-control logpadding"
-                                        name="email" value={this.state.email}
-                                        onChange={this.onChange}
-                                        placeholder="Enter Email" required /> <br />
+                                           name="email" value={this.state.email}
+                                           onChange={this.onChange}
+                                           placeholder="Enter Email" required/> <br/>
 
                                     <input type="text" className="form-control logpadding"
-                                        name="mobile" value={this.state.mobile}
-                                        onChange={this.onChange}
-                                        placeholder="Enter Mobile" required /> <br />
+                                           name="mobile" value={this.state.mobile}
+                                           onChange={this.onChange}
+                                           placeholder="Enter Mobile" required/> <br/>
 
                                     <input type="password" className="form-control logpadding"
-                                        name="password" value={this.state.password}
-                                        onChange={this.onChange}
-                                        placeholder="Enter Password" required /> <br />
+                                           name="password" value={this.state.password}
+                                           onChange={this.onChange}
+                                           placeholder="Enter Password" required/> <br/>
 
                                     <input type="password" className={`form-control logpadding ${password_matched}`}
-                                        name="password2" value={this.state.password2}
-                                        onChange={this.onChange}
-                                        placeholder="Confirm Password" required /> <br />
+                                           name="password2" value={this.state.password2}
+                                           onChange={this.onChange}
+                                           placeholder="Confirm Password" required/> <br/>
                                 </div>
                             </div>
                             <div className="register-c2">
@@ -123,17 +124,17 @@ class Register extends Component {
 }
 
 
-Register.propTypes  =   {
+Register.propTypes = {
     registerUser: PropTypes.func.isRequired,
     auth: PropTypes.object.isRequired,
     errors: PropTypes.object.isRequired
 };
 
 const mapStateToProps = state => ({
-  auth: state.auth,
-  errors: state.errors
+    auth: state.auth,
+    errors: state.errors
 });
 
-const mapDispatchToProps = { registerUser };
+const mapDispatchToProps = {registerUser};
 
-export default connect(mapStateToProps, mapDispatchToProps) (withRouter(Register));
+export default connect(mapStateToProps, mapDispatchToProps)(withRouter(Register));
