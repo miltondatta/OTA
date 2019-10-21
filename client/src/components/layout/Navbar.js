c07d563da707a997b5bc5dfc128cba76653ac3c7
shuvo.babl <shuvo.babl@gmail.com>
2019-10-21 16:34:06 +0600

    nav bar fixed and responsive

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -1,232 +1,77 @@
 import React from 'react';
-import {fade, makeStyles} from '@material-ui/core/styles';
-import {AppBar, Toolbar, IconButton,Typography, InputBase, Badge, MenuItem, Menu, Button} from '@material-ui/core';
-import { spacing } from '@material-ui/system';
-
-import MailIcon from '@material-ui/icons/Mail';
-import SearchIcon from '@material-ui/icons/Search';
-import MoreIcon from '@material-ui/icons/MoreVert';
+import {makeStyles} from '@material-ui/core/styles';
+import Grid from '@material-ui/core/Grid';
+import IconButton from '@material-ui/core/IconButton';
 import AccountCircle from '@material-ui/icons/AccountCircle';
-import NotificationsIcon from '@material-ui/icons/Notifications';
-import {Link} from 'react-router-dom';
+import Menu from '@material-ui/core/Menu';
 
+
+import {Link} from 'react-router-dom';
 import logo from '../../assets/img/logo.png';
 
 const useStyles = makeStyles(theme => ({
-    grow: {
+    root: {
         flexGrow: 1,
     },
-    menuButton: {
-        marginRight: theme.spacing(2),
-    },
-    title: {
-        display: 'none',
-        fontSize:'17px',
-        [theme.breakpoints.up('sm')]: {
-            display: 'block',
-        },
-    },
-    search: {
-        position: 'relative',
-        borderRadius: theme.shape.borderRadius,
-        backgroundColor: fade(theme.palette.common.white, 0.15),
-        '&:hover': {
-            backgroundColor: fade(theme.palette.common.white, 0.25),
-        },
-        marginRight: theme.spacing(2),
-        marginLeft: 0,
-        width: '100%',
-        [theme.breakpoints.up('sm')]: {
-            marginLeft: theme.spacing(3),
-            width: 'auto',
-        },
-    },
-    searchIcon: {
-        width: theme.spacing(7),
-        height: '100%',
-        position: 'absolute',
-        pointerEvents: 'none',
-        display: 'flex',
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-    inputRoot: {
-        color: 'inherit',
-    },
-    inputInput: {
-        padding: theme.spacing(1, 1, 1, 7),
-        transition: theme.transitions.create('width'),
-        width: '100%',
-        [theme.breakpoints.up('md')]: {
-            width: 200,
-        },
-    },
-    sectionDesktop: {
-        display: 'none',
-        [theme.breakpoints.up('md')]: {
-            display: 'flex',
-        },
-    },
-    sectionMobile: {
-        display: 'flex',
-        [theme.breakpoints.up('md')]: {
-            display: 'none',
-        },
-    },
-    headerMenu:{
-        fontSize:'17px',
-        color:'#ffffff',
-        textDecoration:'none',
-        marginRight:'8px'
-    },
-    titleName: {
-        display: 'none',
-        fontSize:'17px',
-        marginRight:'8px',
-        [theme.breakpoints.up('sm')]: {
-            display: 'block',
-        },
-    },
 }));
+
 
 export default function PrimarySearchAppBar() {
     const classes = useStyles();
     const [anchorEl, setAnchorEl] = React.useState(null);
-    const [mobileMoreAnchorEl, setMobileMoreAnchorEl] = React.useState(null);
+    const open = Boolean(anchorEl);
 
-    const isMenuOpen = Boolean(anchorEl);
-    const isMobileMenuOpen = Boolean(mobileMoreAnchorEl);
-
-    const handleProfileMenuOpen = event => {
+    const handleMenu = event => {
         setAnchorEl(event.currentTarget);
     };
 
-    const handleMobileMenuClose = () => {
-        setMobileMoreAnchorEl(null);
-    };
-
-    const handleMenuClose = () => {
+    const handleClose = () => {
         setAnchorEl(null);
-        handleMobileMenuClose();
     };
 
-    const handleMobileMenuOpen = event => {
-        setMobileMoreAnchorEl(event.currentTarget);
-    };
-
-    const menuId = 'primary-search-account-menu';
-    const renderMenu = (
-        <Menu
-            anchorEl={anchorEl}
-            anchorOrigin={{vertical: 'top', horizontal: 'right'}}
-            id={menuId}
-            keepMounted
-            transformOrigin={{vertical: 'top', horizontal: 'right'}}
-            open={isMenuOpen}
-            onClose={handleMenuClose}>
-            <MenuItem onClick={handleMenuClose}>Profile</MenuItem>
-            <MenuItem onClick={handleMenuClose}>My account</MenuItem>
-            <MenuItem onClick={handleMenuClose}>Logout</MenuItem>
-        </Menu>
-    );
-
-    const mobileMenuId = 'primary-search-account-menu-mobile';
-    const renderMobileMenu = (
-        <Menu
-            anchorEl={mobileMoreAnchorEl}
-            anchorOrigin={{vertical: 'top', horizontal: 'right'}}
-            id={mobileMenuId}
-            keepMounted
-            transformOrigin={{vertical: 'top', horizontal: 'right'}}
-            open={isMobileMenuOpen}
-            onClose={handleMobileMenuClose}>
-            <MenuItem>
-                <IconButton aria-label="show 4 new mails" color="inherit">
-                    <Badge badgeContent={4} color="secondary">
-                        <MailIcon/>
-                    </Badge>
-                </IconButton>
-                <p>Messages</p>
-            </MenuItem>
-            <MenuItem>
-                <IconButton aria-label="show 11 new notifications" color="inherit">
-                    <Badge badgeContent={11} color="secondary">
-                        <NotificationsIcon/>
-                    </Badge>
-                </IconButton>
-                <p>Notifications</p>
-            </MenuItem>
-            <MenuItem onClick={handleProfileMenuOpen}>
-                <IconButton
-                    aria-label="account of current user"
-                    aria-controls="primary-search-account-menu"
-                    aria-haspopup="true"
-                    color="inherit">
-                    <AccountCircle/>
-                </IconButton>
-                <p>Profile</p>
-            </MenuItem>
-        </Menu>
-    );
 
     return (
-        <div className={classes.grow}>
-            <AppBar position="static">
-                <Toolbar>
+        <div className={classes.root}>
+            <Grid container className="nav-header">
+                <Grid item sm={3}>
                     <Link to={'/'}><img src={logo} alt="Travel Agency Logo" className="logo"/></Link>
+                </Grid>
 
-                    <div className={classes.search}>
-                        <div className={classes.searchIcon}>
-                            <SearchIcon/>
-                        </div>
-                        <InputBase
-                            placeholder="Search…"
-                            classes={{
-                                root: classes.inputRoot,
-                                input: classes.inputInput,
-                            }}
-                            inputProps={{'aria-label': 'search'}}
-                        />
-                    </div>
-                    <div className={classes.grow}/>
-
-                    <Typography  className={classes.titleName} variant="h6" >
-                        Hello, Shuvo
-                    </Typography>
-
-                    <Typography variant="h6" noWrap>
-                        <Link to={'#'} className={classes.headerMenu}>Home</Link>
-                    </Typography>
-
-                    <Typography variant="h6" noWrap>
-                        <Link to={'/login'} className={classes.headerMenu}>Login</Link>
-                    </Typography>
-
-                    <div className={classes.sectionDesktop}>
+                <Grid item sm={9} className={'menu-bar'}>
+                    <ul>
+                        <li><Link to={'/'}>Home</Link></li>
+                        <li><Link to={'/contact'}>Contact</Link></li>
+                        <li><Link to={'/login'}>Login</Link></li>
+                    </ul>
+                    <div>
                         <IconButton
-                            edge="end"
                             aria-label="account of current user"
-                            aria-controls={menuId}
+                            aria-controls="menu-appbar"
                             aria-haspopup="true"
-                            onClick={handleProfileMenuOpen}
-                            color="inherit">
+                            onClick={handleMenu}
+                            color="inherit" style={{marginTop: '4px'}}>
                             <AccountCircle/>
                         </IconButton>
+                        <Menu
+                            id="menu-appbar"
+                            anchorEl={anchorEl}
+                            anchorOrigin={{
+                                vertical: 'top',
+                                horizontal: 'right',
+                            }}
+                            keepMounted
+                            transformOrigin={{
+                                vertical: 'top',
+                                horizontal: 'right',
+                            }}
+                            open={open}
+                            onClose={handleClose}>
+                            <li><Link class={'profile-menu'} to={'/contact'}>Profile</Link></li>
+                            <li><Link class={'profile-menu'} to={'/logout'}>Logout</Link></li>
+                        </Menu>
                     </div>
-                    <div className={classes.sectionMobile}>
-                        <IconButton
-                            aria-label="show more"
-                            aria-controls={mobileMenuId}
-                            aria-haspopup="true"
-                            onClick={handleMobileMenuOpen}
-                            color="inherit">
-                            <MoreIcon/>
-                        </IconButton>
-                    </div>
-                </Toolbar>
-            </AppBar>
-            {renderMobileMenu}
-            {renderMenu}
+                </Grid>
+            </Grid>
         </div>
     );
 }
