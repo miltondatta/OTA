63b2b94fdb85be5ada217a8f9a420415bde7eb27
plaboncosta <plabonjoseph@gmail.com>
2019-10-22 14:44:44 +0600

    register page design done

--- client/src/components/contact/Contact.js
+++ client/src/components/contact/Contact.js
@@ -3,55 +3,57 @@ import {Form, Button, Card} from "react-bootstrap";
 
 // Css
 import '../../assets/css/contact.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
 
 class Contact extends Component {
     render() {
         return (
             <div className="contact-area">
-                <div className="container contact-area-container">
-                    <div className="row">
-                        <div className="col-md-8 offset-md-2">
-                            <Card>
-                                <Card.Header>
-                                    <h3>Contact Us</h3>
-                                </Card.Header>
-                                <Card.Body>
-                                    <Form>
-                                        <Form.Row>
-                                            <Form.Group className="col-md-6"
-                                                        controlId="formGridEmail">
-                                                <Form.Label>Name</Form.Label>
-                                                <Form.Control type="text" placeholder="Enter Name" required/>
-                                            </Form.Group>
+                <div className="contact-overlay">
+                    <div className="container contact-area-container">
+                        <div className="row">
+                            <div className="col-md-8 offset-md-2">
+                                <Card>
+                                    <Card.Header>
+                                        <h3>Contact Us</h3>
+                                    </Card.Header>
+                                    <Card.Body>
+                                        <Form>
+                                            <Form.Row>
+                                                <Form.Group className="col-md-6"
+                                                            controlId="formGridEmail">
+                                                    <Form.Label>Name</Form.Label>
+                                                    <Form.Control type="text" placeholder="Enter Name" required/>
+                                                </Form.Group>
 
-                                            <Form.Group className="col-md-6 pr-0"
-                                                        controlId="formGridPassword">
-                                                <Form.Label>Phone</Form.Label>
-                                                <Form.Control type="text" placeholder="Enter Phone Number" required/>
-                                            </Form.Group>
-                                        </Form.Row>
+                                                <Form.Group className="col-md-6 pr-0"
+                                                            controlId="formGridPassword">
+                                                    <Form.Label>Phone</Form.Label>
+                                                    <Form.Control type="text" placeholder="Enter Phone Number"
+                                                                  required/>
+                                                </Form.Group>
+                                            </Form.Row>
 
-                                        <Form.Row>
-                                            <Form.Group className="col-md-6"
-                                                        controlId="formGridEmail">
-                                                <Form.Label>Email</Form.Label>
-                                                <Form.Control type="email" placeholder="Enter email" required/>
-                                            </Form.Group>
+                                            <Form.Row>
+                                                <Form.Group className="col-md-6"
+                                                            controlId="formGridEmail">
+                                                    <Form.Label>Email</Form.Label>
+                                                    <Form.Control type="email" placeholder="Enter email" required/>
+                                                </Form.Group>
 
-                                            <Form.Group className="col-md-6 pr-0"
-                                                        controlId="formGridPassword">
-                                                <Form.Label>Address</Form.Label>
-                                                <Form.Control type="text" placeholder="Enter Address" required/>
-                                            </Form.Group>
-                                        </Form.Row>
+                                                <Form.Group className="col-md-6 pr-0"
+                                                            controlId="formGridPassword">
+                                                    <Form.Label>Address</Form.Label>
+                                                    <Form.Control type="text" placeholder="Enter Address" required/>
+                                                </Form.Group>
+                                            </Form.Row>
 
-                                        <Button variant="info" type="submit">
-                                            Submit
-                                        </Button>
-                                    </Form>
-                                </Card.Body>
-                            </Card>
+                                            <Button variant="info" type="submit">
+                                                Submit
+                                            </Button>
+                                        </Form>
+                                    </Card.Body>
+                                </Card>
+                            </div>
                         </div>
                     </div>
                 </div>
