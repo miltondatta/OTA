e7c89d6def5e878fb8bcc4b9293afa8f2ddf4359
plaboncosta <plabonjoseph@gmail.com>
2019-10-22 18:29:48 +0600

    menubar done

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -1,9 +1,7 @@
 import React, {Fragment} from 'react';
-import {makeStyles} from '@material-ui/core/styles';
-import Grid from '@material-ui/core/Grid';
-import IconButton from '@material-ui/core/IconButton';
-import AccountCircle from '@material-ui/icons/AccountCircle';
-import Menu from '@material-ui/core/Menu';
+import {Navbar, Nav, NavDropdown} from "react-bootstrap";
+
+// Redux
 import Proptypes from 'prop-types';
 import {connect} from "react-redux";
 import {logoutUser} from "../../actions/authActions";
@@ -11,87 +9,49 @@ import {logoutUser} from "../../actions/authActions";
 import {Link} from 'react-router-dom';
 import logo from '../../assets/img/logo.png';
 
-const useStyles = makeStyles(theme => ({
-    root: {
-        flexGrow: 1,
-    },
-}));
-
-
-function Navbar({logoutUser, auth: {isAuthenticated}}) {
-    const classes = useStyles();
-    const [anchorEl, setAnchorEl] = React.useState(null);
-    const open = Boolean(anchorEl);
-
-    const handleMenu = event => {
-        setAnchorEl(event.currentTarget);
-    };
-
-    const handleClose = () => {
-        setAnchorEl(null);
-    };
-
+function Menubar({logoutUser, auth: {isAuthenticated, user}}) {
     const guestLinks = (
         <Fragment>
-            <li><Link to={'/contact'}>Contact</Link></li>
-            <li><Link to={'/login'}>Login</Link></li>
-            <li><Link to={'/register'}>Register</Link></li>
+            <li className="nav-item"><Link className="nav-link" to="/">Home</Link></li>
+            <li className="nav-item"><Link className="nav-link" to="/login">Login</Link></li>
+            <li className="nav-item"><Link className="nav-link" to="/register">Register</Link></li>
+            <li className="nav-item"><Link className="nav-link" to="/contact">Contact</Link></li>
+        </Fragment>
+    );
+
+    const authLinks = (
+        <Fragment>
+            <li className="nav-item"><Link to="#" className={'nav-link'}>Hi, {user.name}</Link></li>
         </Fragment>
     );
 
     return (
-        <div className={classes.root}>
-            <Grid container className="nav-header">
-                <Grid item sm={3}>
-                    <Link to={'/'}><img src={logo} alt="Travel Agency Logo" className="logo"/></Link>
-                </Grid>
-
-                <Grid item sm={9} className={'menu-bar'}>
-                    <ul>
-                        <li><Link to={'/'}>Home</Link></li>
-                        {!isAuthenticated && guestLinks}
-                    </ul>
+        <Navbar collapseOnSelect expand="lg" variant="dark">
+            <Link className="navbar-brand" to="/">
+                <img src={logo} alt="Travel Agency Logo" className="logo"/>
+            </Link>
+            <Navbar.Toggle aria-controls="responsive-navbar-nav"/>
+            <Navbar.Collapse id="responsive-navbar-nav">
+                <Nav className="ml-auto">
+                    {!isAuthenticated && guestLinks}
+                    {isAuthenticated && authLinks}
                     {isAuthenticated &&
-                    <div>
-                        <IconButton
-                            aria-label="account of current user"
-                            aria-controls="menu-appbar"
-                            aria-haspopup="true"
-                            onClick={handleMenu}
-                            color="inherit" style={{marginTop: '4px'}}>
-                            <AccountCircle/>
-                        </IconButton>
-                        <Menu
-                            id="menu-appbar"
-                            anchorEl={anchorEl}
-                            anchorOrigin={{
-                                vertical: 'top',
-                                horizontal: 'right',
-                            }}
-                            keepMounted
-                            transformOrigin={{
-                                vertical: 'top',
-                                horizontal: 'right',
-                            }}
-                            open={open}
-                            onClose={handleClose}>
-                            <li><Link className={'profile-menu'} to={'/profile'}>Profile</Link></li>
-                            <li><Link className={'profile-menu'} to={'#'} onClick={e => {
-                                e.preventDefault();
-                                logoutUser();
-                                setAnchorEl(null);
-                            }}>Logout</Link></li>
-                        </Menu>
-                    </div>
+                    <NavDropdown title="Profile" id="collasible-nav-dropdown">
+                        <Link to={'/profile'} className={'dropdown-item'}>Profile</Link>
+                        <Link to={'#'} className={'dropdown-item'} onClick={e => {
+                            e.preventDefault();
+                            logoutUser();
+                        }}>Logout</Link>
+                    </NavDropdown>
                     }
-                </Grid>
-            </Grid>
-        </div>
+                </Nav>
+            </Navbar.Collapse>
+        </Navbar>
     );
 }
 
 
-Navbar.propTypes = {
+Menubar.propTypes = {
     logoutUser: Proptypes.func.isRequired,
     auth: Proptypes.object.isRequired
 };
@@ -101,4 +61,4 @@ const mapStateToProps = state => ({
 });
 
 const mapDispatchToProps = {logoutUser};
-export default connect(mapStateToProps, mapDispatchToProps)(Navbar);
+export default connect(mapStateToProps, mapDispatchToProps)(Menubar);
