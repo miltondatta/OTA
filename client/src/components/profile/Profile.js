e0fc45c910132cc71e371d904e317ea73e9fe37d
shuvo.babl <shuvo.babl@gmail.com>
2019-10-22 18:05:29 +0600

    add profile page and value

--- client/src/components/profile/Profile.js
+++ client/src/components/profile/Profile.js
@@ -1,24 +1,92 @@
 import React, {Component} from 'react';
-import {Tabs,Tab} from "react-bootstrap";
+import Proptypes from 'prop-types';
+import {connect} from "react-redux";
+import {Tabs, Tab, Form, Button, Card} from "react-bootstrap";
 import '../../assets/css/profile.css'
 
 class Profile extends Component {
 
-    render() {
-        return (
-            <div className="container profile-area-container">
-                <Tabs defaultActiveKey="home" transition={false} id="noanim-tab-example">
-                    <Tab eventKey="home" title="Home">
-                        
-                    </Tab>
-                    <Tab eventKey="profile" title="Profile">
+    constructor(props) {
+        super(props);
+        this.state = {
+            name: '',
+            email: '',
+            mobile: '',
+        };
 
-                    </Tab>
-                </Tabs>
+         this.onChange = this.onChange.bind(this);
+        // this.onSubmit = this.onSubmit.bind(this);
+    }
+
+    render() {
+        const {isAuthenticated,user} = this.props.auth;
+        return (
+            <div className="register-area">
+                <div className="register-overlay">
+                    <div className="container profile-area-container">
+                        <Tabs defaultActiveKey="home" transition={false} id="noanim-tab-example">
+                            <Tab eventKey="home" title="Profile">
+                                <Form>
+                                    <Form.Row>
+                                        <Form.Group controlId="formGridName"
+                                                    className={'col-md-6 col-sm-12'}>
+                                            <Form.Label>Name</Form.Label>
+                                            <Form.Control type="text" placeholder="Enter Name" value={user.name} onChange={this.onChange}/>
+                                        </Form.Group>
+                                        <Form.Group controlId="formGridEmail"
+                                                    className={'col-md-6 col-sm-12'}>
+                                            <Form.Label>Email</Form.Label>
+                                            <Form.Control type="email" placeholder="Enter email" value={user.email}/>
+                                        </Form.Group>
+                                        <Form.Group controlId="formGridMobileNo"
+                                                    className={'col-md-6 col-sm-12'}>
+                                            <Form.Label>Mobile No</Form.Label>
+                                            <Form.Control type="mobile" placeholder="Enter Mobile No" value={user.mobile}/>
+                                        </Form.Group>
+                                    </Form.Row>
+                                </Form>
+                            </Tab>
+                            <Tab eventKey="password" title="Password">
+                                <Form>
+                                    <Form.Row>
+                                        <Form.Group controlId="formGridCurrentPass"
+                                                    className={'col-md-6 col-sm-12'}>
+                                            <Form.Label>Current Password</Form.Label>
+                                            <Form.Control type="password"
+                                                          placeholder="Enter Current Password"/>
+                                        </Form.Group>
+                                    </Form.Row>
+                                    <Form.Row>
+                                        <Form.Group controlId="formGridNewPass"
+                                                    className={'col-md-6 col-sm-12'}>
+                                            <Form.Label>New Password</Form.Label>
+                                            <Form.Control type="password"
+                                                          placeholder="Enter New Password"/>
+                                        </Form.Group>
+                                        <Form.Group controlId="formGridConfirmPass"
+                                                    className={'col-md-6 col-sm-12'}>
+                                            <Form.Label>Confirm Password</Form.Label>
+                                            <Form.Control type="password"
+                                                          placeholder="Enter Confirm Password"/>
+                                        </Form.Group>
+                                    </Form.Row>
+                                    <Button type="submit">Change Password</Button>
+                                </Form>
+                            </Tab>
+                        </Tabs>
+                    </div>
+                </div>
             </div>
         );
     }
-
 }
 
-export default Profile;
+Profile.propTypes = {
+    auth: Proptypes.object.isRequired
+};
+
+const mapStateToProps = state => ({
+    auth: state.auth
+});
+
+export default connect(mapStateToProps)(Profile);
