9b13b3fc851954598ea83501fce53f3c94bc2b16
plaboncosta <plabonjoseph@gmail.com>
2019-10-23 11:25:31 +0600

    design footer done

--- client/src/components/layout/Footer.js
+++ client/src/components/layout/Footer.js
@@ -1,57 +1,68 @@
 import React from 'react';
-import {makeStyles} from '@material-ui/core/styles';
-import Grid from '@material-ui/core/Grid';
-import footer_logo from "../../assets/img/footer-logo.png";
 import {Link} from 'react-router-dom';
-import '../../assets/css/footer.css';
-import {ChevronRight} from '@material-ui/icons';
 
-const useStyles = makeStyles(theme => ({
-    root: {
-        flexGrow: 1,
-    },
-    footerLogo: {
-        width: '140px !important',
-    }
-}));
+// Font Awesome
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faAngleRight} from "@fortawesome/free-solid-svg-icons";
+
+// Css
+import '../../assets/css/footer.css';
+
+// Image
+import footer_logo from "../../assets/img/footer-logo.png";
+import facebook from '../../assets/img/icon-facebook.png';
+import twitter from '../../assets/img/icon-twitter.png';
+import google_plus from '../../assets/img/icon-gplus.png';
+import youtube from '../../assets/img/icon-youtube.png';
 
 export default function CenteredGrid() {
-    const classes = useStyles();
 
     return (
-        <div className={classes.root}>
-            <Grid container className="footerbgblack">
-                <Grid item md={3} sm={6} xs={12} className="gridSinglePortion">
-                    <Link to={'/'}><img className={classes.footerLogo} src={footer_logo} alt=""/></Link><br/>
-                    <span className="grey2">© 2019  |  <Link className={'privacy'} to={'/'}>Privacy Policy</Link><br/>
-                                All Rights Reserved </span>
-                </Grid>
-                <Grid item md={3} sm={6} xs={12} className="gridSinglePortion">
-                    <span className="ftitleblack">Customer support</span><br/>
-                    <span className="pnr">1-866-599-6674</span><br/>
-                    <span className="grey2">office@travel.com</span>
-                </Grid>
-                <Grid item md={3} sm={6} xs={12} className="gridSinglePortion">
-                    <span className="ftitleblack">Parnerships</span>
-                    <br/>
-                    <ul className="footerlistblack">
-                        <li><ChevronRight className="footerArrow"/><Link to={'/'}>Weddings</Link></li>
-                        <li><ChevronRight className="footerArrow"/><Link to={'/'}>Accessible Travel</Link></li>
-                    </ul>
-                </Grid>
-                <Grid item md={3} sm={6} xs={12} className="gridSinglePortion">
-                    <div className="scont">
-                        <Link to={'/'} className="social1b"><img src="../../travel/images/icon-facebook.png"
-                                                                 alt=""/></Link>
-                        <Link to={'/'} className="social2b"><img src="../../travel/images/icon-twitter.png"
-                                                                 alt=""/></Link>
-                        <Link to={'/'} className="social3b"><img src="../../travel/images/icon-gplus.png"
-                                                                 alt=""/></Link>
-                        <Link to={'/'} className="social4b"><img src="../../travel/images/icon-youtube.png"
-                                                                 alt=""/></Link>
+        <div className={'footerbgblack'}>
+            <div className={'container'}>
+                <div className="row">
+                    <div className="col-lg-3 col-md-6 com-sm-6 col-12 gridSinglePortion">
+                        <Link to={'/'}>
+                            <img className="footerLogo" src={footer_logo} alt=""/>
+                        </Link><br/>
+                        <span className="grey2">© 2019  |  <Link className={'privacy'} to={'/'}>Privacy Policy</Link>
+                        <br/>
+                    All Rights Reserved </span>
                     </div>
-                </Grid>
-            </Grid>
+
+                    <div className="col-lg-3 col-md-6 com-sm-6 col-12 gridSinglePortion customer-support">
+                        <span className="ftitleblack">Customer support</span><br/>
+                        <span className="pnr">1-866-599-6674</span><br/>
+                        <span className="grey2">office@travel.com</span>
+                    </div>
+
+                    <div className="col-lg-3 col-md-6 com-sm-6 col-12 gridSinglePortion partnership">
+                        <span className="ftitleblack">Partnerships</span>
+                        <br/>
+                        <ul className="footerlistblack">
+                            <li><FontAwesomeIcon icon={faAngleRight} /><Link to={'/'}>Weddings</Link></li>
+                            <li><FontAwesomeIcon icon={faAngleRight} /><Link to={'/'}>Accessible Travel</Link></li>
+                        </ul>
+                    </div>
+
+                    <div className="col-lg-3 col-md-6 com-sm-6 col-12 gridSinglePortion socials">
+                        <div className="scont">
+                            <Link to={'/'} className="social1b">
+                                <img src={facebook} alt=""/>
+                            </Link>
+                            <Link to={'/'} className="social2b">
+                                <img src={twitter} alt=""/>
+                            </Link>
+                            <Link to={'/'} className="social3b">
+                                <img src={google_plus} alt=""/>
+                            </Link>
+                            <Link to={'/'} className="social4b">
+                                <img src={youtube} alt=""/>
+                            </Link>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
