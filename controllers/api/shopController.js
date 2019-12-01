1eba431e5dba86df0dc57e7b621554ed2a59eba4
Shahadat Hossain <shahadat@clouditbd.com>
2019-12-01 19:14:31 +0600

    update shop controller

--- controllers/api/shopController.js
+++ controllers/api/shopController.js
@@ -10,7 +10,7 @@ const airport    = require('../../models').airport;
 const airline    = require('../../models').airline; 
 
 exports.shop = async (req, res) => {
-    //Prepare data from file 
+ /*   //Prepare data from file 
     let shopData            = [];
     let iatas               = [];
     let airlines            = [];
@@ -130,7 +130,33 @@ exports.shop = async (req, res) => {
             console.log(err);
         }
     });
+    return;
+    
+    */
+
+
+
+
+   /*  
+    const flightInfo_param = {
+        airline: 'LH',
+        flightNumber: '2551',
+        departure: '2019-12-04',
+    };
+
+    travelport.flightInfo(flightInfo_param).then(
+        data => fs.writeFile("api_output/travelport/flightInfo2.txt", JSON.stringify(data, null, 4), (err) => {
+            console.log("start_writing");
+            console.log(data);
+            console.log("End Writing");
+        }), err => fs.writeFile("api_output/travelport/flightInfo2.txt", JSON.stringify(err, null, 4), (err) => {
+            console.log("err start_writing");
+            console.log(err);
+            console.log("err End Writing");
+        }) 
+     );
     return; 
+        */
 
 
 
@@ -154,63 +180,64 @@ exports.shop = async (req, res) => {
         cabins: [cabins],
         pricing   : {
             currency: 'USD'
-        },
+        }
     };  
 
 
-    /*
+    
     //For travelport - Response preparation
     travelport.shop(params)
     .then(
         apiRes => {
-            let shopData   = [];
-            //let parseData  = JSON.parse(apiRes);
-
-            for(let i = 0; i < apiRes.length; i++){
-                let directions = apiRes[i].directions;
-                directions.forEach(flight => {
-                    for(let j = 0; j < flight.length; j++) {                
-                        let flightData = {};
-                        flightData['source']            = 1;
-                        flightData['totalPrice']        = apiRes[i].totalPrice;
-                        flightData['basePrice']         = apiRes[i].basePrice;
-                        flightData['taxes']             = apiRes[i].taxes;
-                        flightData['from']              = flight[j].from;
-                        flightData['to']                = flight[j].to;
-                        flightData['from_city']         = flight[j].from;
-                        flightData['to_city']           = flight[j].to;
-                        flightData['platingCarrier']    = flight[j].platingCarrier;
-                        let dataSegments                = flight[j].segments; 
-                        let segments     =  [];
-                        for(let k = 0; k < dataSegments.length; k++) {
-                            let segment = {};
-                            segment['from']         = dataSegments[k].from;
-                            segment['to']           = dataSegments[k].to;   
-                            segment['departure']    = dataSegments[k].departure;   
-                            segment['arrival']      = dataSegments[k].arrival;   
-                            segment['airline']      = dataSegments[k].airline;   
-                            segment['flightNumber'] = dataSegments[k].flightNumber;   
-                            segment['duration']     = dataSegments[k].duration[0];  
-                            segment['bookingClass'] = dataSegments[k].bookingClass;   
-                            segment['baggage']      = dataSegments[k].baggage[0].amount + ' ' + dataSegments[k].baggage[0].units;   
-                            segments.push(segment);
-                        }
-                        flightData['segments']      =   segments;
-                        shopData.push(flightData);
-                    }              
-                    
+                console.log("API Response");
+                fs.writeFile("api_output/travelport/shop.txt", JSON.stringify(apiRes, null, 4), (err) => {
+                    console.log("Successfully Written to File.");
                 });
-            } 
-            let response            = {};
-            response['status']      = true;
-            response['message']     = 'Successfully process your request!';
-            response['data']        = shopData;
-            return res.status(200).json(response);
+
+            const fromSegments = apiRes['0'].directions['0']['0'].segments;
+           // const toSegments   = apiRes['0'].directions['1']['0'].segments;
+            const book = {
+                segments: fromSegments,
+                rule: 'SIP',
+                passengers: [{
+                    lastName: 'SKYWALKER',
+                    firstName: 'ANAKIN',
+                    passCountry: 'UA',
+                    passNumber: 'ES221731',
+                    birthDate: '1968-07-25',
+                    gender: 'M',
+                    ageCategory: 'ADT',
+                }],
+                phone: {
+                    countryCode: '38',
+                    location: 'IEV',
+                    number: '0660419905',
+                },
+                deliveryInformation: {
+                    name: 'Anakin Skywalker',
+                    street: 'Sands street, 42',
+                    zip: '42042',
+                    country: 'Galactic Empire',
+                    city: 'Mos Eisley',
+                },
+                allowWaitlist: true,
+            };
+
+            console.log("Call Booking Request");
+
+            travelport.book(book).then(
+                res => fs.writeFile("api_output/travelport/book.txt", JSON.stringify(res, null, 4), (res) => {
+                    console.log("Successfully Written to File.");
+                }),
+                err => fs.writeFile("api_output/travelport/book.txt", JSON.stringify(err, null, 4), (err) => {
+                    console.log("Error Written to File.");
+                })
+            );
         },
         apiErr => {
             return res.status(200).json({ 'status': false, 'message': 'There is a problem in your request, please try again later!' });
         }
-    ); */
+    ); 
 
 
 
