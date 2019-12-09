0c62b358af1ba031490cbb34e751fc3467bda9be
Shahadat Hossain <shahadat@clouditbd.com>
2019-12-09 12:49:50 +0600

    remove cache

--- client/src/actions/errorActions.js
+++
@@ -1,9 +0,0 @@
-import {GET_ERRORS} from "./types";
-
-// Empty Errors
-export const emptyError = () => dispatch => {
-    dispatch({
-        type: GET_ERRORS,
-        payload: {}
-    })
-};
--- client/src/utils/Urls.js
+++
@@ -1,4 +0,0 @@
-export const base_url = 'http://localhost:3000/';
-export const shopApi  = '/api/air/shop';
-export const register = '/api/users/register';
-export const login    = '/api/users/login';
