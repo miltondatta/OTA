ac77e4ea1509a0186b48083360477eb1168a0faf
shuvo.babl <shuvo.babl@gmail.com>
2019-12-23 18:17:26 +0600

    added airlines info

--- migrations/20191120063755-create-airline.js
+++ migrations/20191120063755-create-airline.js
@@ -1,43 +1,45 @@
 'use strict';
 module.exports = {
-  up: (queryInterface, Sequelize) => {
-    return queryInterface.createTable('airlines', {
-      id: {
-        allowNull: false,
-        type: Sequelize.INTEGER
-      },
-      name: {
-        type: Sequelize.STRING
-      },
-      alias: {
-        type: Sequelize.STRING
-      },
-      iata: {
-        type: Sequelize.STRING
-      },
-      icao: {
-        type: Sequelize.STRING
-      },
-      callsign: {
-        type: Sequelize.STRING
-      },
-      country: {
-        type: Sequelize.STRING
-      },
-      active: {
-        type: Sequelize.STRING
-      },
-      createdAt: {
-        allowNull: true,
-        type: Sequelize.DATE
-      },
-      updatedAt: {
-        allowNull: true,
-        type: Sequelize.DATE
-      }
-    });
-  },
-  down: (queryInterface, Sequelize) => {
-    return queryInterface.dropTable('airlines');
-  }
+    up  : (queryInterface, Sequelize) => {
+        return queryInterface.createTable('airlines', {
+            id       : {
+                allowNull: false,
+                type     : Sequelize.INTEGER
+            },
+            name     : {
+                type: Sequelize.STRING
+            },
+            alias    : {
+                type: Sequelize.STRING
+            },
+            iata     : {
+                type: Sequelize.STRING
+            },
+            icao     : {
+                type: Sequelize.STRING
+            },
+            callsign : {
+                type: Sequelize.STRING
+            },
+            country  : {
+                type: Sequelize.STRING
+            },
+            active   : {
+                type: Sequelize.STRING
+            },
+            createdAt: {
+                allowNull   : true,
+                type        : Sequelize.DATE,
+                defaultValue: Sequelize.NOW
+            },
+            updatedAt: {
+                allowNull   : true,
+                type        : Sequelize.DATE,
+                defaultValue: Sequelize.NOW
+            }
+        });
+    },
+    down: (queryInterface, Sequelize) => {
+        return queryInterface.dropTable('airlines');
+    }
 };
